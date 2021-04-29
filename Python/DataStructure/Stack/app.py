d5ec6e5ae390b834c5db1e837cb8069fa5c9ef6c
Ali Mojtabavi <mojtabavi99@yahoo.com>
2021-04-29 19:36:13 +0430

    stack with python

---
+++ Python/DataStructure/Stack/app.py
@@ -0,0 +1,26 @@
+
+
+class ArrayStack:
+    def __init__(self):
+        self._data = []
+    def len(self):
+        return len(self._data)
+    def isEmpty(self):
+        return len(self._data) == 0
+    def push(self, e):
+        self._data.append(e)
+    def top(self):
+        if self.isEmpty():
+            return 'Stack is empty.'
+        return self._data[-1]
+    def pop(self):
+        if self.isEmpty():
+            return 'Stack is empty.'
+        return self._data.pop()
+
+s = ArrayStack()
+s.push(2)
+s.push(3)
+s.push(4)
+print(s.pop())
+print(s.pop())
